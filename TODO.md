98568afb65ea618e907147897c42711b70688b8f
barthess <barthess@yandex.ru>
2012-06-18 16:08:50 +0300

    added todo

---
+++ TODO.md
@@ -0,0 +1,6 @@
+change ntp server to openwrt
+fix S10
+fix S25
+add keys to /root/.ssh
+add netkeeper
+
--- batch_ssh.sh
+++ batch_ssh.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-IP="frame13    frame164   frame175   frame51    frame61"
+IP="frame23    frame13    frame164   frame175   frame51    frame61"
 
 ####################################################
 # main cycle
 for ip in $IP
 do
-	ssh $ip "echo 'openwrt' > /etc/ntpserver"
+	# scp S11netkeep.sh $ip:/etc/init.d/ 
+	ssh  $ip '/etc/init.d/S11netkeep.sh'
 	if [[ $? == "" ]]
 	then
 		echo "$ip done"
