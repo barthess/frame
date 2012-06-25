479a3b99b81607d9fef1a11edb86b392a5c81bb6
barthess <barthess@yandex.ru>
2012-06-25 09:26:44 +0300

    nop

--- batch_ssh.sh
+++ batch_ssh.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-IP="frame23    frame13    frame164   frame175   frame51    frame61"
+IP="frame23 frame13 frame164 frame175 frame51 frame61 frame52 frame62"
 
 ####################################################
 # main cycle
 for ip in $IP
 do
-	# scp S11netkeep.sh $ip:/etc/init.d/ 
-	ssh  $ip 'rm /etc/init.d/S11netkeep.sh'
+	scp S12netkeep.sh $ip:/root/
+	ssh $ip '/root/S12netkeep.sh'
 	if [[ $? == "" ]]
 	then
 		echo "$ip done"
